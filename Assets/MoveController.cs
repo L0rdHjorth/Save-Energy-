5f3c8afe8d6d4875f963c9785fbaa093db1137c4
MasterIwan <jesperiwanjorgensen@gmail.com>
2022-09-30 23:23:01 +0200

    yes

--- Assets/MoveController.cs
+++ Assets/MoveController.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine.Rendering.Universal;
 
 public class MoveController : MonoBehaviour
 {
     private CircleCollider2D circleCollider;
+    int lysSlukket; //antal lys slukket
 
     [SerializeField] float speed;
     
     // Start is called before the first frame update
     void Start()
     {
+        print("hello world");
         circleCollider = GetComponent<CircleCollider2D>();
+        lysSlukket = 0;
     }
 
     // Update is called once per frame
@@ -48,15 +53,33 @@ public class MoveController : MonoBehaviour
 
     public void slukLys()
     {
-        GameObject[] lysArray = GameObject.FindGameObjectsWithTag("Lys"); // Alt lys bliver sat ind i lysArray
+        GameObject[] lysArray = GameObject.FindGameObjectsWithTag("Lys"); // Alt lys bliver sat ind i lysArray 
+
         foreach (GameObject lys in lysArray)
         {
             if (Vector3.Distance(lys.transform.position, transform.position) <= 1f) // lyset slukket hvis spilleren er tæt nok på
             {
+                print(lysArray.Length);
+                if (lys.GetComponent<Light2D>().intensity != 0)
+                {
+                    lysSlukket++;
+
+                }
                 lys.GetComponent<Light2D>().intensity = 0;
+            }
+            if (lysArray.Length == lysSlukket)
+            {
+                GameOver();
+                print("lys end");
             }
         }
 
+    }//slukLys end 
+    public void GameOver()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetActiveScene());
+        print("END");
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 
 }
