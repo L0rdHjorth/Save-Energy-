93639ba744959daf5318b8461003f73026cd6e21
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2022-10-01 13:52:23 +0200

    Sound script

---
+++ Assets/LydVedAktion.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LydVedAktion : MonoBehaviour
+{
+    [SerializeField] AudioClip clip;
+    [SerializeField] AudioSource source;
+    [SerializeField] GameObject linkedLight;
+    bool hasSpoken = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasSpoken)
+        {
+            if (linkedLight.GetComponent<Light2D>().intensity == 0)
+            {
+                source.PlayOneShot(clip);
+                hasSpoken = true;
+            }
+        }
+
+    }
+}
