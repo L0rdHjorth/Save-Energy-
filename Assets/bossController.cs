0abc99cbd549274278f4ee2df55a341ee61cb708
L0rdHjorth <114767172+L0rdHjorth@users.noreply.github.com>
2022-10-01 19:41:50 +0200

    boss ai working

---
+++ Assets/bossController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float Tid;
+    GameObject[] Flashlights;
+    [SerializeField] GameObject player;
+    public float speed;
+    void Start()
+    {
+        Flashlights = GameObject.FindGameObjectsWithTag("Flashlight");
+        player = GameObject.FindGameObjectWithTag("Player");
+        foreach (GameObject flashlight in Flashlights)
+        {
+            flashlight.transform.eulerAngles += new Vector3(0, 0, 90);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector2.Distance(transform.position, player.transform.position);
+        Tid += Time.deltaTime;
+        Vector2 direction = player.transform.position - transform.position;
+
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (dist < 4)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+      
+        
+    }
+}
