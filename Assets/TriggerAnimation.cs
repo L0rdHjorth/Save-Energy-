0c1ef99d52e205ae60fb74ff9b4ffe9dfe79f361
L0rdHjorth <114767172+L0rdHjorth@users.noreply.github.com>
2022-10-01 13:37:07 +0200

    animation trigger now works for desk

--- Assets/TriggerAnimation.cs
+++ Assets/TriggerAnimation.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class TriggerAnimation : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    Animator Desk;
+    [SerializeField] GameObject Player;
+    public GameObject[] deskarray;
+    Animator Deskanime;
     void Start()
     {
-        Desk = GetComponent<Animator>();
+        Deskanime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,11 +21,22 @@ public class TriggerAnimation : MonoBehaviour
         {
             animationstart();
         }
-        
+
     }
 
     public void animationstart()
     {
-        Desk.SetInteger("state", 1);
+        GameObject[] deskarray = GameObject.FindGameObjectsWithTag("Desk");
+
+
+        foreach (GameObject Desk in deskarray)
+        {
+            if (Vector3.Distance(Desk.transform.position,Player.transform.position) <= 1f)
+            {
+                Deskanime.SetInteger("state", 1);
+
+
+            }
+        }
     }
 }
