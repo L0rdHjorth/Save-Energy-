68ea3eee234b52697b33550243807230a5cc9cbd
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2022-10-02 02:06:26 +0200

    Animation til lvl 2 monitor

--- Assets/BossDoorLock.cs
+++ Assets/BossDoorLock.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BossDoorLock : MonoBehaviour
 {
-    bool DoorsAreUnlocked = false;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/IsChildDying.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class IsChildDying : MonoBehaviour
+{
+    [SerializeField] GameObject linkedLight;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (linkedLight.GetComponent<Light2D>().intensity == 0)
+        {
+            animator.SetBool("IsDead", true);
+            print("denne del virker");
+        }
+    }
+}
